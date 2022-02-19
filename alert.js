41347a1091a6fa1c67f0964484eccd692f3393d2
james122790 <71894283+james122790@users.noreply.github.com>
2022-02-19 13:00:29 +0000

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,1 @@
-alert("Be sure to include what department you are wanting to contact if you have a specific question for them. Our staff will get back with via Email as soon as possible.");
+alert("Be sure to include what department you are wanting to contact if you have a specific question for them. Our staff will get back with you via Email as soon as possible.");
