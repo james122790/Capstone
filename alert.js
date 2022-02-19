1361035701bb97a7eb2f961fd5167465cb85a3fc
james122790 <71894283+james122790@users.noreply.github.com>
2022-02-19 12:57:35 +0000

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,1 @@
-alert("Welcome to LPG Groups new site!");
+alert("Be sure to include what department you are wanting to contact if you have a specific question for them. Our staff will get back with via Email as soon as possible.");
